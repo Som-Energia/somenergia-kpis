479ca4e0a22e100bd8bbc04f718cfaa4ee6cd3ba
Roger <roger.sanjaume@somenergia.coop>
2021-12-30 15:59:00 +0100

    add click, day, month meff updates

--- main.py
+++ main.py
@@ -1,6 +1,50 @@
-from meff.meff_operations import update_closing_prices
+import sys
+import click
+from meff.meff_operations import (
+    update_closing_prices_day,
+    update_closing_prices_month
+)
+# from omie.omie_operations import get_historical_hour_price
 
+
+function_list = {
+    'update_closing_prices_day': update_closing_prices_day,
+    'update_closing_prices_month': update_closing_prices_month
+}
+
+# TODO use logging instead of -v
+@click.command()
+@click.option('-f','--function', type=str, default=None, help='Choose which function you want to run.', multiple=True)
+@click.option('-v','--verbose', default=2, count=True)
+@click.option('-l','--list-functions', default=False, is_flag=True)
+@click.option('-s','--dry-run', default=False, is_flag=True, help='Show dataframes but dont save to db')
+def dispatch(function, verbose, list_functions, dry_run):
+
+    if list_functions:
+        print(f'Available functions {list(function_list.keys())}')
+        return 0
+
+    results = []
+
+    for one_function in function:
+        operation_function = function_list.get(one_function, None)
+
+        if function:
+            result = operation_function(verbose, dry_run)
+            if result != 0 and verbose > 0 or result and verbose > 1: 
+                print(f'{function} ended with result {result}')
+        else:
+            if verbose > 0:
+                print(f'{function} not found. options: {list(function_list.keys())}')
+            result = -1
+    
+        results.append(result)
+
+    if verbose > 1:
+        print(results)
+    return results
 
 if __name__ == '__main__':
-
-  update_closing_prices()
+    results = dispatch()
+    final_result = min(results)
+    sys.exit(final_result)
--- meff/meff_operations.py
+++ meff/meff_operations.py
@@ -4,7 +4,7 @@ import urllib.request, gzip
 
 from sqlalchemy import create_engine
 
-from dbconfig import helpscout_api, puppis_prod_db, local_superset_db
+from dbconfig import puppis_prod_db, local_superset_db
 
 
 # imports
@@ -12,27 +12,78 @@ from dbconfig import helpscout_api, puppis_prod_db, local_superset_db
 # https://www.meff.es/esp/Derivados-Commodities/Precios-Cierre/Excel
 
 
-def update_closing_prices():
+def update_closing_prices_day(verbose=2, dry_run=False):
 
-    closing_time = datetime.datetime.now(datetime.timezone.utc)
+    request_time = datetime.datetime.now(datetime.timezone.utc)
+    noms_columnes = ['dia','res','base_precio','base_dif','base_dif_per','res','punta_precio','punta_dif','punta_dif_per']
+    if not dry_run:
+        engine = create_engine(local_superset_db['dbapi'])
 
-    print(f'Logging meff closing at {closing_time}')
+    try:
+        precios_cierre = pd.read_html('https://www.meff.es/ing/Commodities-Derivatives/Close-Prices/Excel')[0]
+    except:
+        # TODO handle exceptions
+        raise
 
-    precios_cierre = pd.read_html('https://www.meff.es/esp/Derivados-Commodities/Precios-Cierre')[0]
-    import pdb; pdb.set_trace()
-    precios_cierre.columns = ['dia','res','base_precio','base_dif','base_dif_per','res','punta_precio','punta_dif','punta_dif_per']
-    precios_cierre.drop(columns='res', axis=1, inplace=True)
-    precios_cierre['fetch_datetime'] = closing_time
+    if verbose > 2:
+        print(precios_cierre)
 
-    print(precios_cierre)
+    precios_cierre_dia = precios_cierre\
+        .set_axis(noms_columnes, axis=1)\
+        .drop(columns='res', axis=1)\
+        .set_index('dia', drop=False)\
+        .filter(regex='^Day', axis=0)\
+        .assign(
+            request_time = lambda x: request_time,
+            dia = lambda x: pd.to_datetime(x['dia'], format='Day %d-%b-%Y').dt.date
+        )
 
-    engine = create_engine(local_superset_db['dbapi'])
+    if dry_run:
+        print(precios_cierre_dia)
+    else:
+        try:
+            precios_cierre_dia.to_sql('meff_precios_cierre_dia', engine, index = False, if_exists = 'replace')
+        except:
+            # TODO handle exceptions
+            raise
 
-    precios_cierre.to_sql('meff_precios_cierre', engine, index = False, if_exists = 'replace')
-    
+def update_closing_prices_month(verbose=2, dry_run=False):
+
+    request_time = datetime.datetime.now(datetime.timezone.utc)
+    noms_columnes = ['mes','res','base_precio','base_dif','base_dif_per','res','punta_precio','punta_dif','punta_dif_per']
+    if not dry_run:
+        engine = create_engine(local_superset_db['dbapi'])
+
+    if verbose > 2:
+        print(precios_cierre)
+
+    try:
+        precios_cierre = pd.read_html('https://www.meff.es/ing/Commodities-Derivatives/Close-Prices/Excel')[0]
+    except:
+        # TODO handle exceptions
+        raise
+
+    precios_cierre_mes = precios_cierre\
+        .set_axis(noms_columnes, axis=1)\
+        .drop(columns='res', axis=1)\
+        .set_index('mes', drop=False)\
+        .filter(regex='^Month', axis=0)\
+        .assign(
+            request_time = lambda x: request_time,
+            dia = lambda x: pd.to_datetime(x['mes'], format='Month %b-%Y').dt.date
+        )
+
+    if dry_run:
+        print(precios_cierre_mes)
+    else:
+        try:
+            precios_cierre_mes.to_sql('meff_precios_cierre_mes', engine, index = False, if_exists = 'replace')
+        except:
+            # TODO handle exceptions
+            raise
 
 
-
-
-
-
+def update_las_closing_prices_long(verbose=2, dry_run=False):
+    # To Do: Fitxer amb historic + previsio?
+    # https://www.meff.es/esp/Derivados-Commodities/Ultimos-Precios-Cierre
+    pass
