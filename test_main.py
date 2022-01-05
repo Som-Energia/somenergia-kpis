42f88b6c24a4dec62530a59919dae73dcae88c24
pol <pol.monso@somenergia.coop>
2022-01-05 16:41:43 +0100

    WIP add integration tests

---
+++ test_main.py
@@ -0,0 +1,16 @@
+import unittest
+
+class MainIntegrationTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test__get_historical_energy_buy(self):
+        pass
