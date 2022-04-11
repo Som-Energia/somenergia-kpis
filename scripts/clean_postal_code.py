d456151814c1b97d9a6a725332e4caf6cad9fa95
Alberto <bluesnatural@gmail.com>
2022-04-11 10:52:39 +0200

    Added functions and tests to extract data from erp

--- scripts/clean_postal_code.py
+++ scripts/clean_postal_code.py
@@ -1,7 +1,19 @@
 #!usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def download_data_from_erp(client, model, fields, limit=10):
-    data = client.model(model).search([], limit=limit, fields=fields)
-    return client.model(model).read(data, fields=fields)
+import pandas as pd
 
+def download_data_from_erp(client, **kwargs):
+    data = client.model(kwargs['model']).search([], **kwargs)
+    return client.model(kwargs['model']).read(data, **kwargs)
+
+def erp_to_pandas(erp_data):
+    df = pd.DataFrame(erp_data)
+    return df
+
+def df_to_csv(df, file_name):
+    df.to_csv(file_name, index = False)
+
+def read_csv(file_name):
+    df = pd.read_csv(file_name)
+    return df
--- scripts/clean_postal_code_test.py
+++ scripts/clean_postal_code_test.py
@@ -4,7 +4,12 @@
 import unittest
 import configdb
 from erppeek import Client
-from scripts.clean_postal_code import download_data_from_erp
+from clean_postal_code import (
+    download_data_from_erp,
+    erp_to_pandas,
+    df_to_csv,
+    read_csv,
+)
 
 
 class ExtractDataFromErp_Test(unittest.TestCase):
@@ -12,8 +17,49 @@ class ExtractDataFromErp_Test(unittest.TestCase):
         self.client = Client(**configdb.erppeek_testing)
 
     def test_download_data_from_erp(self):
-        data = download_data_from_erp(self.client, 'res.partner.address', ['name', 'zip'], limit=5)
+        data = download_data_from_erp(self.client,
+        model = 'res.partner.address',
+        fields = ['name', 'zip'])
+
         self.assertIsInstance(data, list)
         self.assertIsInstance(data[0], dict)
         self.assertIn('name', data[0])
         self.assertIn('zip', data[0])
+
+    def test_download_data_from_erp_with_limit(self):
+        data = download_data_from_erp(self.client,
+        model = 'res.partner.address',
+        fields = ['name', 'zip'],
+        limit=5)
+
+        self.assertIsInstance(data, list)
+        self.assertIsInstance(data[0], dict)
+        self.assertIn('name', data[0])
+        self.assertIn('zip', data[0])
+        self.assertEqual(len(data), 5)
+
+    def test_erp_to_pandas(self):
+        data = download_data_from_erp(self.client,
+        model = 'res.partner.address',
+        fields = ['name', 'zip'])
+
+        df = erp_to_pandas(data)
+
+        self.assertIn('name', df.columns)
+        self.assertIn('zip', df.columns)
+        self.assertEqual(len(df), len(data))
+
+    def test_df_to_csv(self):
+        data = download_data_from_erp(self.client,
+        model = 'res.partner.address',
+        fields = ['name', 'zip'])
+
+        df = erp_to_pandas(data)
+
+        df_to_csv(df, 'res_partner.csv')
+
+        df = read_csv('res_partner.csv')
+
+        self.assertIn('name', df.columns)
+        self.assertIn('zip', df.columns)
+        self.assertEqual(len(df), len(data))
