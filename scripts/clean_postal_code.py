0408d76b7ffe72dcdb4d214f5328daba595c0af2
Alberto <bluesnatural@gmail.com>
2022-04-24 10:00:09 +0200

    Added test null false and fix some tests

--- scripts/clean_postal_code.py
+++ scripts/clean_postal_code.py
@@ -8,34 +8,12 @@ from json import loads
 from requests.adapters import HTTPAdapter
 from requests.exceptions import ConnectionError
 
-def download_res_partner_data_from_erp_to_csv(client, model, filename, fields=[]):
+def download_res_partner_data_from_erp_to_csv(client, filename,
+                            model='res.partner.address', fields=[]):
     data = client.model(model).search([])
     data = client.model(model).read(data, fields)
     df = pd.DataFrame(data)
     df.to_csv(filename, index = False)
-    return df
-
-def clean_street_string(df):
-    spec_chars = ["!",'"',"#","%","&","'","(",")",
-                    "*","+",",","-",".","/",":",";","<",
-                    "=",">","?","@","[","\\","]","^","_",
-                    "`","{","|","}","~","-"]
-
-    df['street_clean'] = df['street'].str.replace(r'\d+', '')
-    for char in spec_chars:
-        df['street_clean'] = df['street_clean'].str.replace(char,'')
-    df['street_clean'] = df['street_clean'].str.findall(r'\w{3,}').str.join(' ')
-    return df
-
-def split_id_municipi_by_id_and_name_in_separated_columns(df):
-    spec_chars = ["[","]"]
-    df['id_municipi_copy'] = df['id_municipi']
-    for char in spec_chars:
-        df['id_municipi_copy'] = df['id_municipi_copy'].str.replace(char,'')
-    df['id_municipi_name'] = df['id_municipi_copy'].str.split(r"\d\,", expand=True)[1]
-    df['id_municipi_id']= df['id_municipi_copy'].str.split(r"\,\s[A-Z]{1}", expand=True)[0]
-    df['id_municipi_name'] = df['id_municipi_name'].str.strip()
-    df['id_municipi_id'] = df['id_municipi_id'].str.strip()
     return df
 
 def get_data_zip_candidates_from_cartociudad(df):
@@ -64,27 +42,8 @@ def get_data_zip_candidates_from_cartociudad(df):
             df.loc[index, 'zip_candidate_cartociudad'] = response_list[0]['postalCode']
     return df
 
-def get_df_with_null_and_false_values(df,column_name):
-    df_false_values = df[df[column_name] == 'False']
-    df_null_values = df[df[column_name].isnull()]
-    df_null_false = pd.concat([df_null_values, df_false_values])
-    return df_null_false
-
-def get_df_with_zip_null_and_false_with_street(df):
-    df.loc[
-        (df['zip']=='False')&
-        (df['street_clean']!='False')&
-        (df['street_clean'].notnull())&
-        (df['street_clean']!=''),['zip','street_clean','street']]
-    return df
-
-def get_zips_by_ine_municipio_nombre(df_ine, row_id_municipi_name):
-    df_grouped = df_ine.groupby(['municipio_nombre']).get_group(row_id_municipi_name)
-    candidates = ' '.join(df_grouped['codigo_postal'].tolist())
-    return candidates
-
 def get_data_zip_candidates_from_ine(df, df_ine):
-
+    df_ine, df = get_normalized_zips_from_ine_erp(df_ine,df)
     ine_municipio_nombre_copy = df_ine['municipio_nombre'].to_list()
     ine_codigo_postal_copy = df_ine['codigo_postal'].astype(str).to_list()
 
@@ -97,6 +56,57 @@ def get_data_zip_candidates_from_ine(df, df_ine):
     for index, row in df.iterrows():
         row['zip_candidate_ine'] = get_zips_by_ine_municipio_nombre(df_ine, row['id_municipi_name'])
         df.loc[index, 'zip_candidate_ine'] = row['zip_candidate_ine']
+    return df
+
+def get_df_with_null_and_false_values(df,column_name):
+    df_false_values = df[df[column_name] == 'False']
+    df_null_values = df[df[column_name].isnull()]
+    df_null_false = pd.concat([df_null_values, df_false_values])
+    return df_null_false
+
+def get_df_with_zip_null_and_false_with_street(df):
+    df.loc[
+        (df['zip']=='False')&
+        (df['zip'].isnull())&
+        (df['street_clean']!='False')&
+        (df['street_clean'].notnull())&
+        (df['street_clean']!=''),['zip','street_clean','street']]
+    return df
+
+def get_zips_by_ine_municipio_nombre(df_ine, row_id_municipi_name):
+    df_grouped = df_ine.groupby(['municipio_nombre']).get_group(row_id_municipi_name)
+    candidates = ' '.join(df_grouped['codigo_postal'].tolist())
+    return candidates
+
+def get_normalized_zips_from_ine_erp(df_ine,df):
+    df_ine['codigo_postal'].str.replace(r'[a-zA-Z]*', '', regex=True)
+
+    df_ine['codigo_postal'].loc[
+        (df_ine['codigo_postal'].str.len()==4)] = '0' + df_ine['codigo_postal'].astype(str)
+
+    df['zip'].loc[
+        (df['zip'].str.len()==4)] = '0' + df['zip'].astype(str)
+    return df_ine, df
+
+def get_normalized_name_street_address(df):
+    spec_chars = ["!",'"',"#","%","&","'","(",")",
+                    "*","+",",","-",".","/",":",";","<",
+                    "=",">","?","@","[","\\","]","^","_",
+                    "`","{","|","}","~","-"]
+    transtab = str.maketrans(dict.fromkeys(spec_chars, ''))
+    df['street_clean'] = df['street'].str.replace(r'\d+', '', regex=True)
+    df['street_clean'] = df['street_clean'].str.translate(transtab)
+    df['street_clean'] = df['street_clean'].str.findall(r'\w{3,}').str.join(' ')
+    return df
+
+def split_id_municipi_by_id_and_name(df):
+    spec_chars = ["[","]"]
+    transtab = str.maketrans(dict.fromkeys(spec_chars, ''))
+    df['id_municipi_copy'] = df['id_municipi']
+    df['id_municipi_name'] = df['id_municipi_copy'] \
+        .str.translate(transtab).str.split(r"\d\,", expand=True)[1].str.strip()
+    df['id_municipi_id'] = df['id_municipi_copy'] \
+        .str.translate(transtab).str.split(r"\,\s[A-Z]{1}", expand=True)[0].str.strip()
     return df
 
 # vim: ts=4 sw=4 et
--- scripts/clean_postal_code_test.py
+++ scripts/clean_postal_code_test.py
@@ -9,7 +9,7 @@ from clean_postal_code import (
     get_data_zip_candidates_from_ine,
     download_res_partner_data_from_erp_to_csv,
     get_data_zip_candidates_from_cartociudad,
-    split_id_municipi_by_id_and_name_in_separated_columns
+    split_id_municipi_by_id_and_name
 )
 from erppeek import Client
 
@@ -19,7 +19,7 @@ class ExtractDataFromErp_Test(unittest.TestCase):
         self.client = Client(**dbconfig.erppeek_testing)
 
     def test__download_res_partner_data_from_erp_to_csv(self):
-        df = download_res_partner_data_from_erp_to_csv(self.client,
+        download_res_partner_data_from_erp_to_csv(self.client,
         model = 'res.partner.address',
         filename='res_partner_address.csv',)
         df = pd.read_csv('res_partner_address.csv')
@@ -57,13 +57,13 @@ class LoadAndTransformData_Test(unittest.TestCase):
         df_result = pd.DataFrame(responseq)
         pd.testing.assert_frame_equal(df_expect, df_result)
 
-    def test__split_id_municipi_by_id_and_name_in_separated_columns(self):
+    def test__split_id_municipi_by_id_and_name(self):
         data = {
             'id': [1],
             'id_municipi': ["[5237, Sant Quirze del Vallès]"],
         }
         df = pd.DataFrame(data)
-        df = split_id_municipi_by_id_and_name_in_separated_columns(df)
+        df = split_id_municipi_by_id_and_name(df)
         result = df.iloc[0]
         self.assertEqual(result['id_municipi_id'], '5237')
         self.assertEqual(result['id_municipi_name'], 'Sant Quirze del Vallès')
@@ -90,9 +90,10 @@ class LoadAndTransformData_Test(unittest.TestCase):
                 8054],
         }
         df_ine = pd.DataFrame(data_ine)
+
         data_expected = {
             'zip_is_in_ine': [
-                True, True, False],
+                True, True, True],
         }
         df_expected = pd.DataFrame(data_expected)
 
@@ -127,7 +128,7 @@ class LoadAndTransformData_Test(unittest.TestCase):
             'zip_candidate_ine': [
                 "25560 25567 25568 25569",
                 "17455 17456",
-                "8755",],
+                "08755",],
         }
 
         df_expected = pd.DataFrame(data_expected)
@@ -135,6 +136,31 @@ class LoadAndTransformData_Test(unittest.TestCase):
         df_result = get_data_zip_candidates_from_ine(df_raw, df_ine)
         pd.testing.assert_series_equal(
             df_expected['zip_candidate_ine'], df_result['zip_candidate_ine'])
+
+    def test__get_df_zip_candidates_from_ine__when_zip_start_zero(self):
+        data_raw = {
+            'id': [35],
+            'id_municipi_name': ["Castellbisbal"],
+            'zip':[""]
+        }
+        df_raw = pd.DataFrame(data_raw)
+        data_ine = {
+            'codigo_postal': ["8755",],
+            'municipio_nombre': ["Castellbisbal"],
+            'municipio_id': [8054],
+        }
+        df_ine = pd.DataFrame(data_ine)
+
+        data_expected = {
+            'zip_candidate_ine': ["08755",],
+        }
+
+        df_expected = pd.DataFrame(data_expected)
+
+        df_result = get_data_zip_candidates_from_ine(df_raw, df_ine)
+        pd.testing.assert_series_equal(
+            df_expected['zip_candidate_ine'], df_result['zip_candidate_ine'])
+
 
     def test__get_df_with_null_and_false_values(self):
         data_raw = {
@@ -154,5 +180,34 @@ class LoadAndTransformData_Test(unittest.TestCase):
         df_is_null_false.sort_values('id', inplace=True)
         df_merged_expected.sort_values('id', inplace=True)
         self.assertTrue(df_is_null_false.equals(df_merged_expected))
+
+    def test__get_df_with_zip_null_and_false_with_street(self):
+
+        data_raw = {
+            'zip': ['12345','False', 'False', 'False', None],
+            'id': [1,2,3,4,5],
+            'street_clean': ['Carrer de la Rambla',
+                        'Carrer de la Rambla',
+                        'Carrer de la Rambla',
+                        'Carrer de la Rambla',
+                        'Carrer de la Rambla'],
+        }
+        df_raw = pd.DataFrame(data_raw)
+
+        df_raw = df_raw.set_index('id')
+        data_merged_expected = {
+            'zip': ['False', 'False', 'False', None],
+            'id': [2,3,4,5],
+            'street_clean': ['Carrer de la Rambla',
+                        'Carrer de la Rambla',
+                        'Carrer de la Rambla',
+                        'Carrer de la Rambla'],
+        }
+        df_merged_expected = pd.DataFrame(data_merged_expected)
+        df_merged_expected = df_merged_expected.set_index('id')
+        df_is_null_false = get_df_with_null_and_false_values(df_raw, 'zip')
+        df_is_null_false = df_is_null_false.sort_values('id')
+        df_merged_expected = df_merged_expected.sort_values('id')
+        pd.testing.assert_frame_equal(df_is_null_false, df_merged_expected)
 
 # vim: ts=4 sw=4 et
