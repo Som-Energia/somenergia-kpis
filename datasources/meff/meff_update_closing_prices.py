2d4e45270df6f64298dbcfc18ae5d60c2f6913f0
Roger <roger.sanjaume@somenergia.coop>
2022-06-10 13:32:02 +0200

    Meff update closing prices migrated to airflow-like style

--- datasources/meff/meff_update_closing_prices.py
+++ datasources/meff/meff_update_closing_prices.py
@@ -5,6 +5,8 @@ import pandas as pd
 
 import datetime
 import urllib.request, gzip
+import lxml.html
+import requests
 
 from sqlalchemy import create_engine
 
@@ -14,32 +16,30 @@ from sqlalchemy import create_engine
 # https://www.meff.es/esp/Derivados-Commodities/Precios-Cierre/Excel
 
 
-def get_excel_prices_from_url():
+def get_prices_from_url():
     try:
-        precios_cierre = pd.read_html('https://www.meff.es/ing/Commodities-Derivatives/Close-Prices/Excel')[0]
+        url = 'https://www.meff.es/ing/Commodities-Derivatives/Close-Prices/Excel'
+        precios_cierre = pd.read_html(url)[0]
+        emission_date = lxml.html.fromstring(requests.get(url).text).xpath('//*[@class="tblcaption peq"]')[0].text.strip()
     except:
         # TODO handle exceptions
         raise
+    return precios_cierre, emission_date
 
-def shape_closing_prices(closing_prices: pd.DataFrame):
+def shape_closing_prices(closing_prices: pd.DataFrame, emission_date):
 
     request_time = datetime.datetime.now(datetime.timezone.utc)
-    noms_columnes = ['dia','res','base_precio','base_dif','base_dif_per','res','punta_precio','punta_dif','punta_dif_per']
+    noms_columnes = ['price_date','res','base_precio','base_dif','base_dif_per','res','punta_precio','punta_dif','punta_dif_per']
 
-    closing_day_prices = closing_prices\
+    closing_prices_shaped = closing_prices\
         .set_axis(noms_columnes, axis=1)\
-        .drop(columns='res', axis=1)\
-        .set_index('dia', drop=False)\
-        .filter(regex='^Day', axis=0)\
-        .assign(
-            request_time = lambda x: request_time,
-            dia = lambda x: pd.to_datetime(x['dia'], format='Day %d-%b-%Y').dt.date
-        )
+        .assign(emission_date=emission_date, create_time=request_time)\
+        .drop(columns='res', axis=1)
 
-    return closing_day_prices
+    return closing_prices_shaped
 
-def save_to_db(engine, closing_day_prices: pd.DataFrame):
-    closing_day_prices.to_sql('meff_precios_cierre_dia', engine, index = False, if_exists = 'append')
+def save_to_db(engine, closing_prices_shaped: pd.DataFrame):
+    closing_prices_shaped.to_sql('meff_closing_prices', engine, index = False, if_exists = 'append')
 
 def update_closing_prices(dbapi, dry_run=False):
 
@@ -47,17 +47,17 @@ def update_closing_prices(dbapi, dry_run=False):
     dry_run = False
 
     engine = create_engine(dbapi)
-    closing_day_prices_raw = get_excel_prices_from_url()
-    closing_day_prices = shape_closing_prices(closing_day_prices_raw)
+    closing_prices_raw, emission_date = get_prices_from_url()
+    closing_prices = shape_closing_prices(closing_prices_raw, emission_date)
 
     if dry_run:
-        logging.info(closing_day_prices)
+        logging.info(closing_prices)
     else:
-        save_to_db(engine, closing_day_prices)
+        save_to_db(engine, closing_prices)
 
-    return closing_day_prices
+    return closing_prices
 
 if __name__ == '__main__':
     dbapi = sys.argv[1]
-    dry_run = sys.argv[2] if sys.argc > 1 else False
+    dry_run = sys.argv[2] if len(sys.argv) > 2 else False
     update_closing_prices(dbapi, dry_run)
--- datasources/meff/test_meff_update_closing_prices.py
+++ datasources/meff/test_meff_update_closing_prices.py
@@ -7,6 +7,8 @@ import urllib.request, gzip
 from sqlalchemy import create_engine
 import unittest
 import dbconfig
+import lxml.html
+import requests
 
 from datasources.meff.meff_update_closing_prices import (
     update_closing_prices,
@@ -25,16 +27,17 @@ class MeffOperationsTest(unittest.TestCase):
 
     def test__shape_closing_prices(self):
 
-        raw_prices = pd.read_html('testdata/inputdata/meff_closing_prices.html')
+        raw_prices = pd.read_html('testdata/inputdata/meff_closing_prices.html')[0]
+        emission_date = 'WEDNESDAY, 8 JUNE, 2022'
 
-        goal_prices = pd.read_html('testdata/')
+        meff_df = shape_closing_prices(raw_prices, emission_date)
 
-        meff_df = shape_closing_prices(raw_prices)
+        goal_prices = pd.read_csv('testdata/inputdata/closing_prices_shaped.csv', sep = ';', parse_dates=['create_time'], date_parser=lambda col: pd.to_datetime(col, utc=True))
 
-        assert_frame_equal(meff_df, goal_prices)
+        assert_frame_equal(meff_df.drop(columns='create_time', axis=1), goal_prices.drop(columns='create_time', axis=1))
 
 
-@skipIf(True, "We hate dbs")
+@unittest.skipIf(True, "We hate dbs")
 class MeffOperationsTestTestDB(unittest.TestCase):
 
     def setUp(self):
@@ -44,7 +47,7 @@ class MeffOperationsTestTestDB(unittest.TestCase):
         # TODO destroy db
         pass
 
-    def test__update_closing_prices__base(self):
+    def _test__update_closing_prices__base(self):
 
         dry_run = False
 
