bd12a316c556b8c4465feca73f9ec8bc1a5d7a8f
Roger <roger.sanjaume@somenergia.coop>
2022-06-10 15:21:56 +0200

    Meff slice_closing_prices transformer done

--- pipelines/meff_closing_prices_day_slice.py
+++ pipelines/meff_closing_prices_day_slice.py
@@ -20,29 +20,30 @@ def transform(closing_prices: pd.DataFrame):
     create_time = datetime.datetime.now(datetime.timezone.utc)
 
     closing_prices_transformed = closing_prices\
-        .set_index('dia', drop=False)\
+        .set_index('price_date', drop=False)\
         .filter(regex='^Day', axis=0)\
-        .assign(create_time=create_time)
+        .assign(
+            create_time = create_time,
+            price_date = lambda x: pd.to_datetime(x['price_date'], format='Day %d-%b-%Y').dt.date,
+            emission_date = lambda x: pd.to_datetime(x['emission_date'], format='%A, %d %B, %Y').dt.date,
+        )
 
     return closing_prices_transformed
 
-def load(engine, closing_prices_shaped: pd.DataFrame):
-    closing_prices_shaped.to_sql('meff_closing_prices', engine, index = False, if_exists = 'append')
+def load(engine, closing_prices_shaped: pd.DataFrame, dry_run=False):
+
+    if dry_run:
+        return logging.info(meff_closing_prices)
+
+    closing_prices_shaped.to_sql('meff_closing_prices_day', engine, index = False, if_exists = 'append')
 
 def slice_closing_prices(dbapi, dry_run=False):
 
-    # TODO support dry running
-    dry_run = False
-
     engine = create_engine(dbapi)
 
-    meff_closing_prices = extract()
+    meff_closing_prices = extract(engine)
     meff_closing_prices = transform(meff_closing_prices)
-
-    if dry_run:
-        logging.info(meff_closing_prices)
-    else:
-        load(engine, meff_closing_prices)
+    load(engine, meff_closing_prices,dry_run)
 
     return meff_closing_prices
 
