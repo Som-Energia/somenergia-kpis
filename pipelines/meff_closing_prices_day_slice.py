4735d48f0929e0b56b0a7329baf070e8d5791518
Roger <roger.sanjaume@somenergia.coop>
2022-06-10 14:08:56 +0200

    WIP slice_closing_prices

---
+++ pipelines/meff_closing_prices_day_slice.py
@@ -0,0 +1,52 @@
+from cmath import log
+import sys
+import logging
+
+import pandas as pd
+
+import requests
+
+from sqlalchemy import create_engine
+import datetime
+
+def extract(engine):
+
+    meff_closing_prices = pd.read_sql_table('meff_closing_prices', engine)
+
+    return meff_closing_prices
+
+def transform(closing_prices: pd.DataFrame):
+
+    create_time = datetime.datetime.now(datetime.timezone.utc)
+
+    closing_prices_transformed = closing_prices\
+        .set_index('dia', drop=False)\
+        .filter(regex='^Day', axis=0)\
+        .assign(create_time=create_time)
+
+    return closing_prices_transformed
+
+def load(engine, closing_prices_shaped: pd.DataFrame):
+    closing_prices_shaped.to_sql('meff_closing_prices', engine, index = False, if_exists = 'append')
+
+def slice_closing_prices(dbapi, dry_run=False):
+
+    # TODO support dry running
+    dry_run = False
+
+    engine = create_engine(dbapi)
+
+    meff_closing_prices = extract()
+    meff_closing_prices = transform(meff_closing_prices)
+
+    if dry_run:
+        logging.info(meff_closing_prices)
+    else:
+        load(engine, meff_closing_prices)
+
+    return meff_closing_prices
+
+if __name__ == '__main__':
+    dbapi = sys.argv[1]
+    dry_run = sys.argv[2] if len(sys.argv) > 2 else False
+    slice_closing_prices(dbapi, dry_run)
