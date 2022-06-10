2a2a96aa2ef09c9475cbf510dadc78e07f02d073
Roger <roger.sanjaume@somenergia.coop>
2022-06-10 17:28:30 +0200

    HOTFIX closing_prices_transformed_month_slice support

---
+++ pipelines/meff_closing_prices_month_slice.py
@@ -0,0 +1,54 @@
+from cmath import log
+import sys
+import logging
+
+import pandas as pd
+
+import requests
+
+from sqlalchemy import create_engine
+import datetime
+
+def extract(engine):
+
+    meff_closing_prices = pd.read_sql_table('meff_closing_prices', engine)
+
+    return meff_closing_prices
+
+def transform(closing_prices: pd.DataFrame):
+
+    create_time = datetime.datetime.now(datetime.timezone.utc)
+
+    closing_prices_transformed = closing_prices\
+        .set_index('price_date', drop=False)\
+        .filter(regex='^Month', axis=0)\
+        .assign(
+            create_time = create_time,
+            price_date = lambda x: pd.to_datetime(x['price_date'], format='Month %b-%Y').dt.date,
+            emission_date = lambda x: pd.to_datetime(x['emission_date'], format='%A, %d %B, %Y').dt.date,
+        )\
+        .reset_index(drop=True)
+
+    return closing_prices_transformed
+
+def load(engine, closing_prices_shaped: pd.DataFrame, dry_run=False):
+
+    if dry_run:
+        return logging.info(closing_prices_shaped)
+
+    closing_prices_shaped.to_sql('meff_closing_prices_month', engine, index = False, if_exists = 'append')
+
+def slice_closing_prices(dbapi, dry_run=False):
+
+    engine = create_engine(dbapi)
+
+    meff_closing_prices = extract(engine)
+    meff_closing_prices = transform(meff_closing_prices)
+    load(engine, meff_closing_prices,dry_run)
+
+    return meff_closing_prices
+
+if __name__ == '__main__':
+    dbapi = sys.argv[1]
+    dry_run = sys.argv[2] if len(sys.argv) > 2 else False
+    slice_closing_prices(dbapi, dry_run)
--- pipelines/test_meff_closing_prices_day_slice.py
+++ pipelines/test_meff_closing_prices_day_slice.py
@@ -3,8 +3,9 @@ import unittest
 import pandas as pd
 from pandas.testing import assert_frame_equal
 
-from pipelines.meff_closing_prices_day_slice import (
-    transform,
+from pipelines import (
+    meff_closing_prices_day_slice,
+    meff_closing_prices_month_slice,
 )
 
 class MeffOperationsSliceTest(unittest.TestCase):
@@ -16,16 +17,33 @@ class MeffOperationsSliceTest(unittest.TestCase):
         # TODO destroy db
         pass
 
-    def test__slice_closing_prices(self):
+    def test__slice_closing_prices_day(self):
 
-        raw_prices = pd.read_csv('testdata/inputdata/closing_praces_lake.csv', sep=';', parse_dates=['create_time'], date_parser=lambda col: pd.to_datetime(col, utc=True))
+        raw_prices = pd.read_csv('testdata/inputdata/closing_prices_lake.csv', sep=';', parse_dates=['create_time'], date_parser=lambda col: pd.to_datetime(col, utc=True))
 
-        meff_df = transform(raw_prices)
+        meff_df = meff_closing_prices_day_slice.transform(raw_prices)
 
         # Note: read_csv with date_parser converts each row as element which returns a Timestamp
         # in the transform function we parse the whole Series (column) which returns object dtype
         # Therefore we need to read and then parse otherwise dtypes would differ.
-        goal_prices = pd.read_csv('testdata/inputdata/closing_prices_transformed.csv', sep = ';')\
+        goal_prices = pd.read_csv('testdata/inputdata/closing_prices_transformed_day.csv', sep = ';')\
+            .assign(
+                price_date = lambda x: pd.to_datetime(x.price_date).dt.date,
+                emission_date = lambda x: pd.to_datetime(x.emission_date).dt.date,
+                )
+
+        assert_frame_equal(meff_df.drop(columns='create_time', axis=1), goal_prices.drop(columns='create_time', axis=1))
+
+    def test__slice_closing_prices_month(self):
+
+        raw_prices = pd.read_csv('testdata/inputdata/closing_prices_lake.csv', sep=';', parse_dates=['create_time'], date_parser=lambda col: pd.to_datetime(col, utc=True))
+
+        meff_df = meff_closing_prices_month_slice.transform(raw_prices)
+
+        # Note: read_csv with date_parser converts each row as element which returns a Timestamp
+        # in the transform function we parse the whole Series (column) which returns object dtype
+        # Therefore we need to read and then parse otherwise dtypes would differ.
+        goal_prices = pd.read_csv('testdata/inputdata/closing_prices_transformed_month.csv', sep = ';')\
             .assign(
                 price_date = lambda x: pd.to_datetime(x.price_date).dt.date,
                 emission_date = lambda x: pd.to_datetime(x.emission_date).dt.date,
