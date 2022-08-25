52dcfda5177af6b2bb10b6bd81b1b1a658ce305f
Lugadur <lucia.garcia@somenergia.coop>
2022-08-25 13:48:14 +0200

    db creation, some kpis, and improviment script

--- datasources/erppeek/filtered_models_single_kpis.py
+++ datasources/erppeek/filtered_models_single_kpis.py
@@ -1,66 +1,88 @@
 import sys
 from erppeek import Client
-import pendulum
 import numpy as np
 import pandas as pd
+import ast
 
 # taules todo
-# pilotatge_kpis_description: id, name, description, filter, erp_model, field, function, freq, idempotent, teams, create_date
-# 1, 'Número de factures pendents', 'Factures amb deute fins dl anterior',  'data_venciment < today()', 'Factures_clients_amb_deute', '', 'count', 'daily', 0, 'Cobraments', '2022-08-23'
-
-
+# pilotatge_kpis_description: id, name, description, filter, erp_model, field, function, freq, type_value, teams, create_date
+# 1, 'Número de factures pendents', 'num factures amb deute',  'type = out_invoice AND invoice_id.pending_state.weight > 0 AND data_venciment < today()', 'giscedata.facturacio.factura', '', 'count', 'daily', 0, 'Cobraments', '2022-08-23'
 
 # pilotatge_numeric_kpis: kpi_id, value, create_date
 # 1, 80, '2022-08-24 02:00:00'
 
-def get_kpis_todo(engine, freq, idempotent):
-    query = f'SELECT id, name, filter, erp_model, field, function, freq FROM pilotatge_kpis_description where freq = "{freq}" and indempotent = {idempotent}"'
-    df = pd.read_sql(query, engine)
+def get_kpis_todo(dbapi, freq):
+    query = f"SELECT id, name, filter, erp_model, field, function, freq, type_value FROM pilotatge_kpis_description where freq = '{freq}'"
+    df = pd.read_sql(query, dbapi)
     return df
 
 def calculate_kpi(erp_client, kpi):
+    #import ipdb; ipdb.set_trace()
     obj = erp_client.model(kpi['erp_model'])
-    filtered = obj.search(kpi['filter'])
-
-    raw_values = obj.read(filtered, kpi['field'])
+    
+    filtered = obj.search(ast.literal_eval(kpi['filter']))
 
     if kpi['function'] == 'sum':
-        value = np.sum([t[kpi['field']] for v in raw_values])
-    elif kpis['function'] == 'count':
-        value = len(raw_values)
+        raw_values = obj.read(filtered, kpi['field'])
+        value = round(np.sum(raw_values),4)
+    elif kpi['function'] == 'count':
+        value = len(filtered)
     else:
         raise ValueError(f"kpi: {kpi['name']} id: {kpi['id']} Unknown function {kpi['function']}")
-
-    return kpi['id'], value
+    print(f"Kpi calculated: {kpi['name']}")
+    return kpi['id'], value, kpi['type_value']
 
 def get_kpis(erp_client, kpis_todo):
     kpis_values = []
     for index, kpi in kpis_todo.iterrows():
         kpis_values.append(calculate_kpi(erp_client, kpi))
-
-    df = pd.DataFrame.from_records(kpis_values, columns =['kpi_id', 'value'])
-    df['create_date'] = pendulum.now()
+    
+    df = pd.DataFrame.from_records(kpis_values, columns =['kpi_id', 'value', 'type_value'])
+    df['create_date'] = pd.Timestamp.now().tz_localize(tz='Europe/Madrid')
 
     return df
 
-def update_kpis(engine, erp_client, freq, idempotent):
-
-    todo = get_kpis_todo(engine, freq, idempotent)
+def update_kpis(dbapi, erp_client, freq):
+    
+    todo = get_kpis_todo(dbapi, freq)
     kpis = get_kpis(erp_client, todo)
-    kpis.to_sql('pilotatge_numeric_kpis', engine, if_exists = 'append', index = False)
 
+    kpis\
+        .query("type_value == 'float'")\
+        .drop(columns=['type_value'])\
+        .to_sql('pilotatge_float_kpis', dbapi, if_exists = 'append', index = False)
+    
+    print(f"Float Kpis inserted {str(len(kpis[kpis['type_value']=='float']))} rows")
+    
+    kpis\
+        .query("type_value == 'int'")\
+        .drop(columns=['type_value'])\
+        .to_sql('pilotatge_int_kpis', dbapi, if_exists = 'append', index = False)
+
+    print(f"Integer Kpis inserted {str(len(kpis[kpis['type_value']=='int']))} rows")
+    
 if __name__ == '__main__':
 
-    dbapi = sys.argv[0]
-    freq = sys.argv[1]
-    idempotent = sys.argv[2]
+    dbapi = sys.argv[1]
+    freq = sys.argv[2]
+    erp_server = sys.argv[3]
+    erp_db = sys.argv[4]
+    erp_user = sys.argv[5]
+    erp_password = sys.argv[6]
 
-    if freq not in ['daily','weekly'] or idempotent not in [0,1]:
+    if freq not in ['daily','weekly']:
         raise ValueError('Unknown frequency configuration')
 
-    erp_client = Client() # fix this, is not unique paramater
-    engine = create_engine(dbapi)
-    update_kpis(engine, erp_client, freq, idempotent)
+    erppeek = dict(
+        server=erp_server,
+        db=erp_db,
+        user=erp_user,
+        password=erp_password,
+    )
 
+    erp_client = Client(**erppeek) # fix this, is not unique paramater
+    
+    update_kpis(dbapi, erp_client, freq)
 
+    # python datasources/erppeek/filtered_models_single_kpis.py 'postgresql://luciagd:sql2022@localhost:5432/sandbox' 'daily' 'http://terp01.somenergia.lan:18069' 'somenergia' 'Oriol' 'shah9Nu+'
 
---
+++ datasources/erppeek/pilotatge_kpis_description.sql
@@ -0,0 +1,51 @@
+CREATE TABLE pilotatge_kpis_description (
+    id serial PRIMARY KEY,
+    name VARCHAR ( 50 ) UNIQUE NOT NULL,
+    description VARCHAR ( 700 ),
+    filter VARCHAR ( 1000 ),
+    erp_model VARCHAR ( 50 ),
+    field VARCHAR ( 50 ),
+    function VARCHAR ( 50 ),
+    freq VARCHAR ( 50 ),
+    type_value VARCHAR ( 50 ),
+    teams VARCHAR ( 50 ),
+    create_date timestamp with time zone
+);
+
+CREATE TABLE pilotatge_int_kpis (
+    kpi_id INTEGER,
+    value BIGINT NOT NULL,
+    create_date timestamp with time zone,
+    CONSTRAINT fk_pilotatge_kpi
+        FOREIGN KEY(kpi_id) 
+            REFERENCES pilotatge_kpis_description(id)
+);
+
+CREATE TABLE pilotatge_float_kpis (
+    kpi_id INTEGER,
+    value DECIMAL (100,5) NOT NULL,
+    create_date timestamp with time zone,
+    CONSTRAINT fk_pilotatge_kpi
+        FOREIGN KEY(kpi_id) 
+            REFERENCES pilotatge_kpis_description(id)
+);
+
+INSERT INTO pilotatge_kpis_description(
+	id, name, description, filter, erp_model, field, function, freq, type_value, teams, create_date)
+	VALUES 
+        (1, 'Número de factures pendents', 'num factures amb deute',  '[("type", "=", "out_invoice"), ("invoice_id.pending_state.weight", ">", 0)]', 'giscedata.facturacio.factura', '', 'count', 'daily', 'int', 'Cobraments', NOW()),
+        (2, 'Saldo pendent_a', 'import de les factures amb deute (sumen els "pendent" de les factures amb deute)',  '[("type", "=", "out_invoice"), ("invoice_id.pending_state.weight", ">", 0)]', 'giscedata.facturacio.factura', 'residual', 'sum', 'daily', 'float', 'Cobraments', NOW()),
+        (3, 'Saldo pendent_b', 'import de les extra lines de les factures amb Descripcio = fracció. Aquest resultat és el que queda pendent dels fraccionaments', '[("name", "ilike", "%fraccio"), ("active", "=", "TRUE")]' , 'giscedata.facturacio.extra', 'amount_pending', 'sum', 'daily', 'float', 'Cobraments', NOW()
+        --(5 'Advocats: Import', 'Import de les factures que estan en advocats', '', 'giscedata.facturacio.factura', '', 'sum', 'daily', 'float', 'Cobraments', NOW() 
+        );
+
+
+
+
+
+-- A Factures Client amb deute (giscedata.facturacio.factura)
+    -- 'Pendent' = 'signed_residual' o 'residual'
+    -- 'Total' = 'amount_total' o 'signed_amount_total' o 'saldo'
+
+
+TRUNCATE TABLE pilotatge_int_kpis, pilotatge_float_kpis, pilotatge_kpis_description;
