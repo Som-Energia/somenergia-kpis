fee51667e8068eb26788bbbbc2f6363093977ec4
Roger <roger.sanjaume@somenergia.coop>
2022-08-29 10:00:24 +0200

    WIP

--- datasources/erppeek/filtered_models_single_kpis.py
+++ datasources/erppeek/filtered_models_single_kpis.py
@@ -3,24 +3,37 @@ from erppeek import Client
 import numpy as np
 import pandas as pd
 import ast
+import datetime
 
 # taules todo
-# pilotatge_kpis_description: id, name, description, filter, erp_model, field, function, freq, type_value, teams, create_date
+# pilotatge_kpis_description: id, name, description, filter, erp_model, , context, field, function, freq, type_value, teams, create_date
 # 1, 'Número de factures pendents', 'num factures amb deute',  'type = out_invoice AND invoice_id.pending_state.weight > 0 AND data_venciment < today()', 'giscedata.facturacio.factura', '', 'count', 'daily', 0, 'Cobraments', '2022-08-23'
 
 # pilotatge_numeric_kpis: kpi_id, value, create_date
 # 1, 80, '2022-08-24 02:00:00'
 
 def get_kpis_todo(dbapi, freq):
-    query = f"SELECT id, name, filter, erp_model, field, function, freq, type_value FROM pilotatge_kpis_description where freq = '{freq}'"
+    query = f"SELECT id, name, filter, erp_model, context, field, function, freq, type_value FROM pilotatge_kpis_description where freq = '{freq}'"
     df = pd.read_sql(query, dbapi)
     return df
 
+def filter_string_to_list(filter):
+
+    filter = filter.replace('__today__', str(datetime.datetime.today().date()))
+
+    #if 'lot_actual' in filter:
+    #    id = erp_client.model(lot).search(obert)
+    #    filter = filter.replace('lot_actual', id)
+
+    return ast.literal_eval(filter)
+
 def calculate_kpi(erp_client, kpi):
-    #import ipdb; ipdb.set_trace()
     obj = erp_client.model(kpi['erp_model'])
-    
-    filtered = obj.search(ast.literal_eval(kpi['filter']))
+    import ipdb; ipdb.set_trace()
+    filter = filter_string_to_list(kpi['filter'])
+    cxt = ast.literal_eval(kpi['context'])
+    filtered = obj.search(filter, context = cxt)
+
 
     if kpi['function'] == 'sum':
         raw_values = obj.read(filtered, kpi['field'])
@@ -36,14 +49,14 @@ def get_kpis(erp_client, kpis_todo):
     kpis_values = []
     for index, kpi in kpis_todo.iterrows():
         kpis_values.append(calculate_kpi(erp_client, kpi))
-    
+
     df = pd.DataFrame.from_records(kpis_values, columns =['kpi_id', 'value', 'type_value'])
     df['create_date'] = pd.Timestamp.now().tz_localize(tz='Europe/Madrid')
 
     return df
 
 def update_kpis(dbapi, erp_client, freq):
-    
+
     todo = get_kpis_todo(dbapi, freq)
     kpis = get_kpis(erp_client, todo)
 
@@ -51,16 +64,16 @@ def update_kpis(dbapi, erp_client, freq):
         .query("type_value == 'float'")\
         .drop(columns=['type_value'])\
         .to_sql('pilotatge_float_kpis', dbapi, if_exists = 'append', index = False)
-    
+
     print(f"Float Kpis inserted {str(len(kpis[kpis['type_value']=='float']))} rows")
-    
+
     kpis\
         .query("type_value == 'int'")\
         .drop(columns=['type_value'])\
         .to_sql('pilotatge_int_kpis', dbapi, if_exists = 'append', index = False)
 
     print(f"Integer Kpis inserted {str(len(kpis[kpis['type_value']=='int']))} rows")
-    
+
 if __name__ == '__main__':
 
     dbapi = sys.argv[1]
@@ -77,7 +90,7 @@ if __name__ == '__main__':
         server=erp_server,
         db=erp_db,
         user=erp_user,
-        password=erp_password,
+        password=erp_password
     )
 
     erp_client = Client(**erppeek) # fix this, is not unique paramater
--- datasources/erppeek/pilotatge_kpis_description.sql
+++ datasources/erppeek/pilotatge_kpis_description.sql
@@ -4,6 +4,7 @@ CREATE TABLE pilotatge_kpis_description (
     description VARCHAR ( 700 ),
     filter VARCHAR ( 1000 ),
     erp_model VARCHAR ( 50 ),
+    context VARCHAR ( 50 ),
     field VARCHAR ( 50 ),
     function VARCHAR ( 50 ),
     freq VARCHAR ( 50 ),
@@ -31,15 +32,24 @@ CREATE TABLE pilotatge_float_kpis (
 );
 
 INSERT INTO pilotatge_kpis_description(
-	id, name, description, filter, erp_model, field, function, freq, type_value, teams, create_date)
+	id, name, description, filter, erp_model, context, field, function, freq, type_value, teams, create_date)
 	VALUES
-        (1, 'Número de factures pendents', 'num factures amb deute',  '[("type", "=", "out_invoice"), ("invoice_id.pending_state.weight", ">", 0)]', 'giscedata.facturacio.factura', '', 'count', 'daily', 'int', 'Cobraments', NOW()),
-        (2, 'Saldo pendent_a', 'import de les factures amb deute (sumen els "pendent" de les factures amb deute)',  '[("type", "=", "out_invoice"), ("invoice_id.pending_state.weight", ">", 0)]', 'giscedata.facturacio.factura', 'residual', 'sum', 'daily', 'float', 'Cobraments', NOW()),
-        (3, 'Saldo pendent_b', 'import de les extra lines de les factures amb Descripcio = fracció. Aquest resultat és el que queda pendent dels fraccionaments', '[("name", "ilike", "%fraccio"), ("active", "=", "TRUE")]' , 'giscedata.facturacio.extra', 'amount_pending', 'sum', 'daily', 'float', 'Cobraments', NOW())
-        (4, 'Advocats: Import', 'Import de les factures que estan en advocats', '[("type", "=", "out_invoice"), ("invoice_id.pending_state.weight", ">", 0), ("pending_state", "ilike", "%advocats")]', 'giscedata.facturacio.factura', 'residual', 'sum', 'daily', 'float', 'Cobraments', NOW()
-        );
+        (1, 'Número de factures pendents', 'Num factures amb deute',  '[("type", "=", "out_invoice"), ("invoice_id.pending_state.weight", ">", 0), ("date_due", "<", "__today__" )]', 'giscedata.facturacio.factura', '{"type":"out_invoice"}', '', 'count', 'daily', 'int', 'Cobraments', NOW()),
+        (2, 'Saldo pendent_a', 'Import de les factures amb deute (sumen els "pendent" de les factures amb deute)',  '[("type", "=", "out_invoice"), ("invoice_id.pending_state.weight", ">", 0)]', 'giscedata.facturacio.factura','{"type":"out_invoice"}', 'residual', 'sum', 'daily', 'float', 'Cobraments', NOW()),
+        (3, 'Saldo pendent_b', 'Import de les extra lines de les factures amb Descripcio = fracció. Aquest resultat és el que queda pendent dels fraccionaments', '[("name", "ilike", "%fraccio"), ("active", "=", "TRUE")]' , 'giscedata.facturacio.extra', '{"active_test": False}', 'amount_pending', 'sum', 'daily', 'float', 'Cobraments', NOW()),
+        (4, 'Advocats: Import', 'Import de les factures que estan en advocats amb data venciment avui', '[("type", "=", "out_invoice"), ("invoice_id.pending_state.weight", ">", 0), ("pending_state", "ilike", "%advocats")]', 'giscedata.facturacio.factura', '{"type":"out_invoice"}', 'residual', 'sum', 'daily', 'float', 'Cobraments', NOW()),
+        (5, 'Advocats: Numero factures', 'Num de factures que estan en advocats amb data venciment avui', '[("type", "=", "out_invoice"), ("invoice_id.pending_state.weight", ">", 0), ("pending_state", "ilike", "%advocats")]', 'giscedata.facturacio.factura', '{"type":"out_invoice"}', '', 'count', 'daily', 'int', 'Cobraments', NOW()),
+        (6, 'R1: Import', 'Import de les factures que estan en R1 amb data venciment avui', '[("type", "=", "out_invoice"), ("invoice_id.pending_state.weight", ">", 0), ("pending_state", "ilike", "%r1")]', 'giscedata.facturacio.factura', '{"type":"out_invoice"}', 'residual', 'sum', 'daily', 'int', 'Cobraments', NOW()),
+        (7, 'R1: Número de factures', 'Num de factures que estan en R1 amb data venciment avui', '[("type", "=", "out_invoice"), ("invoice_id.pending_state.weight", ">", 0), ("pending_state", "ilike", "%r1")]', 'giscedata.facturacio.factura', '{"type":"out_invoice"}', '', 'count', 'daily', 'int', 'Cobraments', NOW()),
+        (8, 'Pobresa: Import', 'Import de les factures que estan en pobresa energètica amb data venciment avui', '[("type", "=", "out_invoice"), ("invoice_id.pending_state.weight", ">", 0), ("pending_state", "ilike", "%energetica")]', 'giscedata.facturacio.factura', '{"type":"out_invoice"}', 'residual', 'sum', 'daily', 'int', 'Cobraments', NOW()),
+        (9, 'Pobresa: Número de factures', 'Num de factures que estan en pobresa energètica amb data venciment avui', '[("type", "=", "out_invoice"), ("invoice_id.pending_state.weight", ">", 0), ("pending_state", "ilike", "%energetica")]', 'giscedata.facturacio.factura', '{"type":"out_invoice"}', '', 'count', 'daily', 'int', 'Cobraments', NOW()),
+        (10, 'Fraccionament: Import_a', 'Import de les extra lines de les factures amb Descripcio = fracció. Aquest resultat és el que queda pendent dels fraccionaments', '[("name", "ilike", "%fraccio"), ("active", "=", "TRUE")]' , 'giscedata.facturacio.extra', '{"active_test": False}', 'amount_pending', 'sum', 'daily', 'float', 'Cobraments', NOW()),
+        (11, 'Fraccionament: Import_b', 'Import de les factures que estan en estat pendent "fracció"', '[("type", "=", "out_invoice"), ("invoice_id.pending_state.weight", ">", 0), ("pending_state", "ilike", "%fraccio")]', 'giscedata.facturacio.factura', '{"type":"out_invoice"}', 'residual', 'sum', 'daily', 'int', 'Cobraments', NOW()),
+        (12, 'Fraccionament: Número de factures_b', 'Num de factures que estan en pobresa energètica amb data venciment avui', '[("type", "=", "out_invoice"), ("invoice_id.pending_state.weight", ">", 0), ("pending_state", "ilike", "%fraccio")]', 'giscedata.facturacio.factura', '{"type":"out_invoice"}', '', 'count', 'daily', 'int', 'Cobraments', NOW()),
+        (13, 'Indic Procés - Total F1s amb error', 'F1s amb error dimportació', '[("state", "=", "Error en la importació")]', 'giscedata.facturacio.importacio.linia', '', '', 'count', 'daily', 'int', 'Factura', NOW())
+        (14, 'Indic Procés - Total pòlisses amb incidència', '', '[("state", "=", "Error en la importació")]', 'giscedata.facturacio.lot', '', '', 'count', 'daily', 'int', 'Factura', NOW())
 
-
+;
 
 
 
@@ -49,3 +59,16 @@ INSERT INTO pilotatge_kpis_description(
 
 
 TRUNCATE TABLE pilotatge_int_kpis, pilotatge_float_kpis, pilotatge_kpis_description;
+
+-- Taula de KPIs Pilotatge
+SELECT pkd.name, pkd.description, pfk.value, pfk.create_date
+FROM pilotatge_kpis_description as pkd
+LEFT JOIN pilotatge_float_kpis as pfk
+ON pkd.id = pfk.kpi_id
+where type_value = 'float'
+UNION
+SELECT pkd.name, pkd.description, pik.value, pik.create_date
+FROM pilotatge_kpis_description as pkd
+LEFT JOIN pilotatge_int_kpis as pik
+ON pkd.id = pik.kpi_id
+where type_value = 'int'
