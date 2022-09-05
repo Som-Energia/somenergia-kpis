5bdfb05b6b6605c40c06d2a471bddcde5b62ca14
Lugadur <lucia.garcia@somenergia.coop>
2022-09-05 11:49:32 +0200

    Correct name for kpi 18 and commenting fake tests

--- datasources/erppeek/test_filtered_models_single_kpis.py
+++ datasources/erppeek/test_filtered_models_single_kpis.py
@@ -38,7 +38,7 @@ class FilteredModelsERPTest(unittest.TestCase):
         #    con.execute(query)
         pass
 
-    def test__update_kpis(self):
+    def _test__update_kpis(self):
 
         result = update_kpis(self.puppis_test, self.erp_client, self.freq)
 
@@ -55,7 +55,7 @@ class FilteredModelsERPToDieTest(unittest.TestCase):
 
         self.engine = create_engine(self.puppis_test, echo=True)
 
-    def test__clean_kpis_db(self):
+    def _test__clean_kpis_db(self):
 
         query = 'TRUNCATE TABLE pilotatge_int_kpis, pilotatge_float_kpis, pilotatge_kpis_description;'
         with self.engine.connect() as con:
