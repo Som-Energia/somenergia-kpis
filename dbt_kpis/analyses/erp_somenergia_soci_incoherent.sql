5c68928d74fbce05239e00340b089b015cef245a
pol <pol.monso@somenergia.coop>
2022-09-19 16:36:53 +0200

    cleaning before somenergia_soci

--- dbt_kpis/analyses/erp_somenergia_soci_incoherent.sql
+++ dbt_kpis/analyses/erp_somenergia_soci_incoherent.sql
@@ -1,26 +1,7 @@
 {{ config(materialized='table') }}
 
 
-with socies_incoherents as (
-    SELECT
-        baixa,
-        data_baixa_soci as data_baixa,
-        create_date as data_alta,
-        case
-            when data_baixa_soci is not NULL and baixa = TRUE then TRUE
-            when data_baixa_soci is NULL and baixa = FALSE then TRUE
-            when data_baixa_soci is NULL and baixa = TRUE then FALSE
-            when data_baixa_soci is not NULL and baixa = FALSE then FALSE
-            when baixa = NULL then FALSE
-        end as socia_coherent,
-        case when data_baixa_soci is NULL then TRUE
-			when data_baixa_soci is not NULL and data_baixa_soci between '2009-01-01' and now() + interval '1 day' then TRUE
-			ELSE FALSE
-		end as data_baixa_coherent,
-        create_date between '2009-01-01' and now() + interval '1 day' as data_alta_coherent
-    FROM {{source('datalake_erp', 'somenergia_soci')}}
-)
-select * from socies_incoherents
+select * from {{ref('erp_somenergia_soci_dirty')}}
 where socia_coherent = FALSE or data_baixa_coeherent = FALSE or data_alta_coherent = FALSE
 order by data_alta desc
 LIMIT 1000
--- dbt_kpis/models/erp_transaccional/erp_somenergia_soci.sql
+++ dbt_kpis/models/erp_transaccional/erp_somenergia_soci.sql
@@ -1,26 +1,6 @@
 {{ config(materialized='table') }}
 
-with socies as (
-SELECT
-    baixa as es_baixa,
-    data_baixa_soci as data_baixa,
-    create_date as data_alta,
-    case
-        when data_baixa_soci is not NULL and baixa = TRUE then TRUE
-        when data_baixa_soci is NULL and baixa = FALSE then TRUE
-        when data_baixa_soci is NULL and baixa = TRUE then FALSE
-        when data_baixa_soci is not NULL and baixa = FALSE then FALSE
-        when baixa = NULL then FALSE
-    end as socia_coherent,
-    case when data_baixa_soci is NULL then TRUE
-        when data_baixa_soci is not NULL and data_baixa_soci between '2009-01-01' and now() + interval '1 day' then TRUE
-        ELSE FALSE
-    end as data_baixa_coherent,
-    create_date between '2009-01-01' and now() + interval '1 day' as data_alta_coherent
-FROM {{source('datalake_erp', 'somenergia_soci')}}
-)
-
 select es_baixa, data_baixa, data_alta
-from socies
+from {{ref('erp_somenergia_soci_dirty')}}
 WHERE socia_coherent = TRUE
 and data_baixa_coherent and data_alta_coherent
