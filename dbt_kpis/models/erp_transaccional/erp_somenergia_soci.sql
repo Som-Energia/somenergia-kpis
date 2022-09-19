d8cc560ecd0ba15c1c0200043bc882995a244476
pol <pol.monso@somenergia.coop>
2022-09-19 14:13:59 +0200

    dates incoherents when data_baixa = null

--- dbt_kpis/models/erp_transaccional/erp_somenergia_soci.sql
+++ dbt_kpis/models/erp_transaccional/erp_somenergia_soci.sql
@@ -12,7 +12,10 @@ SELECT
         when data_baixa_soci is not NULL and baixa = FALSE then FALSE
         when baixa = NULL then FALSE
     end as socia_coherent,
-    data_baixa_soci between '2009-01-01' and now() + interval '1 day' as data_baixa_coherent,
+    case when data_baixa_soci is NULL then TRUE
+        when data_baixa_soci is not NULL and data_baixa_soci between '2009-01-01' and now() + interval '1 day' then TRUE
+        ELSE FALSE
+    end as data_baixa_coherent,
     create_date between '2009-01-01' and now() + interval '1 day' as data_alta_coherent
 FROM {{source('datalake_erp', 'somenergia_soci')}}
 )
