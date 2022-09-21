24bb0fc7ef293e9ede842b867882997dd1524f8d
pol <pol.monso@somenergia.coop>
2022-09-21 17:20:52 +0200

    kpi date relative to filter

--- dbt_kpis/models/warehouse/pilotatge_kpis_model.sql
+++ dbt_kpis/models/warehouse/pilotatge_kpis_model.sql
@@ -2,13 +2,13 @@
 
 with kpisvalues as (
 
-    SELECT pkd.id, pkd.name, pkd.description, pkd.freq, pfk.value, pfk.create_date
+    SELECT pkd.id, pkd.name, pkd.filter as erp_filter, pkd.description, pkd.freq, pfk.value, pfk.create_date
     FROM {{ source('erp_operational', 'pilotatge_kpis_description')}} as pkd
     LEFT JOIN {{ source('erp_operational', 'pilotatge_float_kpis')}} as pfk
     ON pkd.id = pfk.kpi_id
     where type_value = 'float'
     UNION ALL
-    SELECT pkd.id, pkd.name, pkd.description, pkd.freq, pik.value, pik.create_date
+    SELECT pkd.id, pkd.name, pkd.filter as erp_filter, pkd.description, pkd.freq, pik.value, pik.create_date
     FROM {{ source('erp_operational', 'pilotatge_kpis_description') }} as pkd
     LEFT JOIN {{ source('erp_operational', 'pilotatge_int_kpis')}} as pik
     ON pkd.id = pik.kpi_id
@@ -23,9 +23,9 @@ kpisvaluesrecoded as (
         description,
         create_date,
         case
-            when freq = '__7_days_ago__' then interval '7 days'
-            when freq = '__3_days_ago__' then interval '3 days'
-            when freq = '__yesterday__' then interval '1 days'
+            when erp_filter ilike '%__7_days_ago__%' then interval '7 days'
+            when erp_filter ilike '%__3_days_ago__%' then interval '3 days'
+            when erp_filter ilike '%__yesterday__%' then interval '1 days'
             else interval '0 days'
         end as day_offset,
 		CASE
@@ -35,7 +35,7 @@ kpisvaluesrecoded as (
 		END as clean_name
 	from kpisvalues
 )
-select clean_name as name, sum(value) as value, string_agg(description, ' , ') as description, create_date - day_offset as kpi_date
+select clean_name as name, sum(value) as value, string_agg(description, ' , ') as description, create_date - day_offset as kpi_date, create_date
 from kpisvaluesrecoded
-group by clean_name, kpi_date
+group by clean_name, kpi_date, create_date
 
