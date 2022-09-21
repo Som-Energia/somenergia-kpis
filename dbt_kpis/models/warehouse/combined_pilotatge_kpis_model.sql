71540dcb6b1de554525767dc2e5d396422898e69
pol <pol.monso@somenergia.coop>
2022-09-21 15:14:29 +0200

    kpi pivot with dbt, doesnt put value, only count

--- dbt_kpis/models/warehouse/combined_pilotatge_kpis_model.sql
+++ dbt_kpis/models/warehouse/combined_pilotatge_kpis_model.sql
@@ -1,6 +1,10 @@
 {{ config(materialized='view') }}
 
-select
-    *
-from crosstab('select kpi_date, name, value from {{ref("pilotatge_kpis_model")}} order by 1,2')
-as ct(kpi_date timestamptz, kpi1 numeric, kpi2 numeric, kpi3 numeric, kpi4 numeric, kpi5 numeric, kpi6 numeric, kpi7 numeric, kpi8 numeric)
+-- select
+--     *
+-- from crosstab('select kpi_date, name, value from {{ref("pilotatge_kpis_model")}} order by 1,2')
+-- as ct(kpi_date timestamptz, kpi1 numeric, kpi2 numeric, kpi3 numeric, kpi4 numeric, kpi5 numeric, kpi6 numeric, kpi7 numeric, kpi8 numeric)
+
+
+
+select kpi_date, {{ dbt_utils.pivot('name', dbt_utils.get_column_values(ref('pilotatge_kpis_model'), 'name')) }} from {{ ref('pilotatge_kpis_model') }} group by kpi_date
