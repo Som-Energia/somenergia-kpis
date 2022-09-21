3896b4f066e186999f31e7d61ac67276b7c477e2
pol <pol.monso@somenergia.coop>
2022-09-21 18:20:17 +0200

    kpis combined via groupby

--- dbt_kpis/models/datamart_pilotatge/pilotatge_kpis.sql
+++ dbt_kpis/models/datamart_pilotatge/pilotatge_kpis.sql
@@ -1,7 +1,9 @@
 {{ config(materialized='view') }}
 
-select
-    *
+select name, value, description, kpi_date, create_date
 from {{ref('pilotatge_kpis_model')}}
+union all
+select name, value, description, kpi_date, create_date
+from {{ ref('combined_pilotatge_kpis')}}
 
 
---
+++ dbt_kpis/models/warehouse/combined_pilotatge_kpis.sql
@@ -0,0 +1,43 @@
+{{ config(materialized='view') }}
+
+-- TODO table the combination
+with combinable_kpis as (
+    select
+    create_date,
+    value,
+    case
+      when name like 'Saldo pendent' or name like 'Número de factures pendents' then +1
+      else -1
+    end as multiplier,
+    case
+      when name in (
+        'Saldo pendent', -- positive
+        'Factures impagades de contractes de baixa: Import',
+        'R1: Import',
+        'Pobresa: Import',
+        'Fraccionament: Import_a',
+        'Fraccionament: Import_b',
+        'Monitori: Import'
+      ) then 'Import factures en procediment de tall: Import'
+      when name in (
+        'Número de factures pendents', -- positive
+        'Factures impagades de contractes de baixa: Número de factures',
+        'R1: Número de factures',
+        'Pobresa: Número de factures',
+        'Fraccionament: Número de factures',
+        'Fraccionament: Import_b',
+        'Monitori: Número de factures'
+      ) then 'Import factures en procediment de tall: Número'
+      else NULL
+    end as kpi_membership
+    from {{ref('pilotatge_kpis_model')}}
+)
+select
+  create_date as kpi_date,
+  create_date,
+  kpi_membership as name,
+  NULL as description,
+  sum(multiplier*value) as value
+from combinable_kpis
+where kpi_membership is not null
+group by create_date, kpi_membership
