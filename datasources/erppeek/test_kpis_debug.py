cbff28078bf9217fb2996130f5e15120e6ccb16c
pol <pol.monso@somenergia.coop>
2022-10-03 13:43:02 +0200

    add distribuidor test

--- datasources/erppeek/test_kpis_debug.py
+++ datasources/erppeek/test_kpis_debug.py
@@ -29,3 +29,13 @@ class QueryERPTest(unittest.TestCase):
         # cxt = "{""type"":""out_invoice""}"
         entries_ids = obj.search(model_filter, context = cxt)
         obj.read(entries_ids)
+
+    def test__query_erppeek_atc_model__cacs_distribuidor(self):
+        model_name = 'giscedata.atc'
+        obj = self.erp_client.model(model_name)
+        model_filter = [("state","=","pending"),("agent_actual","=","10")]
+        cxt = {}
+        # model_filter = "[(""state"", ""="", ""erroni"")]"
+        # cxt = "{""type"":""out_invoice""}"
+        entries_ids = obj.search(model_filter, context = cxt)
+        print(len(entries_ids))
