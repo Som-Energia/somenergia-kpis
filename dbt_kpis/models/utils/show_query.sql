ff8644516dd1975cf5e2477c5333d8b966814edf
pol <pol.monso@somenergia.coop>
2022-10-04 23:12:13 +0200

    util show query head on dbt run

---
+++ dbt_kpis/models/utils/show_query.sql
@@ -0,0 +1,17 @@
+
+{% set show_query %}
+select * from {{ ref('pilotatge_kpis') }}
+limit 10
+{% endset %}
+
+{% set results = run_query(show_query) %}
+
+{# jinja compile errors on jinja commands that expect results #}
+{# see https://docs.getdbt.com/reference/dbt-jinja-functions/execute #}
+{% if execute %}
+{% do results.print_table() %}
+{{ log("", info=True) }}
+{% endif %}
+
+{# jinja expects something to put inside #}
+{{ show_query }}
