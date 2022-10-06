ca3d60064d0b909924d2e33a859a5ecae2365670
Roger <roger.sanjaume@somenergia.coop>
2022-10-06 17:17:14 +0200

    Mix KPIs from other branch until 64

--- dbt_kpis/models/warehouse/kpis_raw.sql
+++ dbt_kpis/models/warehouse/kpis_raw.sql
@@ -3,13 +3,13 @@
 
 SELECT pkd.id, pkd.code, pkd.name, pkd.filter as erp_filter, pkd.description, pkd.freq, pfk.value, pfk.create_date
 FROM {{ source('erp_operational','erppeek_kpis_description')}} as pkd
-LEFT JOIN {{ source('erp_operational', 'pilotatge_float_kpis')}} as pfk
+INNER JOIN {{ source('erp_operational', 'pilotatge_float_kpis')}} as pfk
 ON pkd.id = pfk.kpi_id
 where type_value = 'float'
 UNION ALL
 SELECT pkd.id, pkd.code, pkd.name, pkd.filter as erp_filter, pkd.description, pkd.freq, pik.value, pik.create_date
 FROM {{ source('erp_operational','erppeek_kpis_description')}} as pkd
-LEFT JOIN {{ source('erp_operational', 'pilotatge_int_kpis')}} as pik
+INNER JOIN {{ source('erp_operational', 'pilotatge_int_kpis')}} as pik
 ON pkd.id = pik.kpi_id
 where type_value = 'int'
 order by id
--- dbt_kpis/models/warehouse/kpis_with_derived.sql
+++ dbt_kpis/models/warehouse/kpis_with_derived.sql
@@ -1,12 +1,20 @@
 {{ config(materialized='view') }}
 
+with kpis_wider_asc as (
+  select * from {{ref('kpis_wider')}} order by create_date asc
+)
+
 SELECT
   *,
   cob2 + cob3 as dbt1,
   cob8 + cob9 as dbt2,
   (cob2 + cob3) - cob15 - cob4 - cob6 - cob8 - cob9 - cob13 as dbt3,
-  (cob1) - cob16 - cob5 - cob7 - cob10 - cob14 as dbt4
-FROM {{ref('kpis_wider')}}
+  (cob1) - cob16 - cob5 - cob7 - cob10 - cob14 as dbt4,
+  con2 - lag(con2,1) over() as dbt10,
+  con5 - lag(con5,1) over() as dbt11,
+  con7 - lag(con7,1) over() as dbt12,
+  con9 - lag(con9,1) over() as dbt13
+FROM kpis_wider_asc
 
 -- Saldo pendent (Saldopendent_a + saldo_pendent_b) as DBT1
 -- Fraccionament: Import (Fraccionament: Import_a Fraccionament: Import_b) as DBT2
