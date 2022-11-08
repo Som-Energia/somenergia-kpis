39eb5fbab5dcebc67e71297b4e90c259f241321d
pol <pol.monso@somenergia.coop>
2022-11-08 15:29:31 +0100

    second dag is dag_slice

--- dags/meff_update_closing_prices_dag.py
+++ dags/meff_update_closing_prices_dag.py
@@ -66,10 +66,10 @@ with DAG(dag_id='meff_slice_closing_prices_dag', start_date=datetime(2022,6,9), 
 
     repo_name = 'somenergia-kpis'
 
-    task_check_repo = build_check_repo_task(dag=dag, repo_name=repo_name)
-    task_git_clone = build_git_clone_ssh_task(dag=dag, repo_name=repo_name)
-    task_branch_pull_ssh = build_branch_pull_ssh_task(dag=dag, task_name=['meff_slice_day_closing_prices','meff_slice_month_closing_prices'], repo_name=repo_name)
-    task_update_image = build_update_image_task(dag=dag, repo_name=repo_name)
+    task_check_repo = build_check_repo_task(dag=dag_slice, repo_name=repo_name)
+    task_git_clone = build_git_clone_ssh_task(dag=dag_slice, repo_name=repo_name)
+    task_branch_pull_ssh = build_branch_pull_ssh_task(dag=dag_slice, task_name=['meff_slice_day_closing_prices','meff_slice_month_closing_prices'], repo_name=repo_name)
+    task_update_image = build_update_image_task(dag=dag_slice, repo_name=repo_name)
 
     meff_slice_day_closing_prices_task = DockerOperator(
         api_version='auto',
