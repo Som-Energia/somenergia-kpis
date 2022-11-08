da6a4d411a2c6dfa2a24206beb0048a29c7338d9
pol <pol.monso@somenergia.coop>
2022-11-08 17:54:19 +0100

    wrong task on get_tags

--- dags/hs_get_tags_dag.py
+++ dags/hs_get_tags_dag.py
@@ -34,7 +34,7 @@ with DAG(dag_id='hs_get_tags_dag', start_date=datetime(2022,6, 15), schedule_int
 
     task_check_repo = build_check_repo_task(dag=dag, repo_name=repo_name)
     task_git_clone = build_git_clone_ssh_task(dag=dag, repo_name=repo_name)
-    task_branch_pull_ssh = build_branch_pull_ssh_task(dag=dag, task_name='hs_get_conversations', repo_name=repo_name)
+    task_branch_pull_ssh = build_branch_pull_ssh_task(dag=dag, task_name='hs_get_tags', repo_name=repo_name)
     task_update_image = build_update_image_task(dag=dag, repo_name=repo_name)
 
     get_tags_task = DockerOperator(
