8ef71312d364a7d7e6a134a96ead0ba0f6b95e37
Roger <roger.sanjaume@somenergia.coop>
2023-03-02 15:18:39 +0100

    Renaming calcul optim

---
+++ datasources/erp/calcul_potencia_optima.py
@@ -0,0 +1,22 @@
+import typer
+import pandas as pd
+
+app = typer.Typer()
+
+'postgres://user:pass@puppis.somenergia.lan:5432/bkmdgf'
+
+@app.command()
+def mockup_potencia_optima(puppis_dbapi: str):
+
+    contractes = [1,2,3]
+    pa = [4,2,3]
+    po = [2.9,4,3]
+
+    dfoptimes = pd.DataFrame({'contracte':contractes, 'potencia_contractada':pa, 'potencia_optima':po})
+    #dfoptimes.to_sql(puppis_dbapi)
+    print(dfoptimes)
+
+    return None
+
+if __name__ == '__main__':
+  app()
