d8ce2b0834f6d5500422013d184e7ad0a0b09aa3
Roger <roger.sanjaume@somenergia.coop>
2023-03-02 15:29:56 +0100

    hotfix dbapi puppis on calcul optim

--- datasources/erp/calcul_potencia_optima.py
+++ datasources/erp/calcul_potencia_optima.py
@@ -13,7 +13,7 @@ def mockup_potencia_optima(puppis_dbapi: str):
     po = [2.9,4,3]
 
     dfoptimes = pd.DataFrame({'contracte':contractes, 'potencia_contractada':pa, 'potencia_optima':po})
-    dfoptimes.to_sql(puppis_dbapi)
+    dfoptimes.to_sql('beedata_calcul_potencia_optima', puppis_dbapi, schema='prod', if_exists='replace', index=False)
     print("It's done!")
 
     return None
