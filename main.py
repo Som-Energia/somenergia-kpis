3c23c0d13aee1a7b9055d2c72c8f9bd12410b8fb
Diego Quintana <daquintanav@gmail.com>
2023-03-08 15:52:20 +0100

    add support for poetry (#11)
    * Add a basic pyproject.toml, updated requirements and documentation for using project with poetry.
    
    Co-authored-by: Lucía García Durañona <Lugadur@users.noreply.github.com>
    
    * update README.md
    
    * add loguru and black to dependencies and refactor main.py
    
    black for better formatting
    imports reorganized
    loguru for logging out of the box without configuration
    
    * update logging in main.py
    
    * update poetry installation instructions
    
    Co-authored-by: Pol Monsó Purtí <polmonso@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Lucía García Durañona <Lugadur@users.noreply.github.com>
    Co-authored-by: Pol Monsó Purtí <polmonso@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,147 +1,141 @@
+import datetime
 import sys
-import click
 from pathlib import Path
 
+import click
 from sqlalchemy import create_engine
-from dbconfig import (
-    local_db,
-    directories,
-    helpscout_api
-)
 
-from datasources.meff.meff_operations import (
-    update_closing_prices_day,
-    update_closing_prices_month
-)
+from datasources.helpscout.hs_get_conversations import update_hs_conversations
+from datasources.meff.meff_operations import update_closing_prices_day, update_closing_prices_month
+from datasources.neuroenergia.neuroenergia_operations import update_neuroenergia
 from datasources.omie.omie_operations import (
-    get_historical_hour_price,
-    update_latest_hour_price,
-    update_energy_buy,
     get_historical_energy_buy,
-    update_historical_hour_price
+    get_historical_hour_price,
+    update_energy_buy,
+    update_historical_hour_price,
+    update_latest_hour_price,
 )
-from datasources.neuroenergia.neuroenergia_operations import (
-    update_neuroenergia
-)
+from dbconfig import directories, helpscout_api, local_db
+from pipelines.energy_budget import pipe_hourly_energy_budget
+from pipelines.omie_garantia import pipe_omie_garantia
 
-from datasources.helpscout.hs_get_conversations import (
-    update_hs_conversations
-)
+from loguru import logger
 
-from pipelines.energy_budget import (
-    pipe_hourly_energy_budget
-)
-
-from pipelines.omie_garantia import (
-    pipe_omie_garantia
-)
-
-import datetime
 
 def main_update_closing_prices_month(verbose, dry_run):
-    engine = None if dry_run else create_engine(local_db['dbapi'])
+    engine = None if dry_run else create_engine(local_db["dbapi"])
     return update_closing_prices_month(engine, verbose, dry_run)
 
+
 def main_update_closing_prices_day(verbose, dry_run):
-    engine = None if dry_run else create_engine(local_db['dbapi'])
+    engine = None if dry_run else create_engine(local_db["dbapi"])
     return update_closing_prices_day(engine, verbose, dry_run)
 
+
 def main_update_omie_latest_hour_price(verbose, dry_run):
-    engine = None if dry_run else create_engine(local_db['dbapi'])
+    engine = None if dry_run else create_engine(local_db["dbapi"])
     return update_latest_hour_price(engine, verbose, dry_run)
 
+
 def main_get_historical_hour_price(verbose, dry_run):
-    engine = None if dry_run else create_engine(local_db['dbapi'])
+    engine = None if dry_run else create_engine(local_db["dbapi"])
     return get_historical_hour_price(engine, verbose, dry_run)
 
+
 def main_update_historical_hour_price(verbose, dry_run):
-    engine = create_engine(local_db['dbapi'])
+    engine = create_engine(local_db["dbapi"])
     return update_historical_hour_price(engine, verbose, dry_run)
+
 
 # TODO Pendent de posar a prod
 def main_get_historical_energy_buy(verbose, dry_run):
     raise NotImplemented
-    engine = None if dry_run else create_engine(local_db['dbapi'])
-    omie_pdbc_dir = Path(directories['OMIE_HISTORICAL_PDBC'])
+    engine = None if dry_run else create_engine(local_db["dbapi"])
+    omie_pdbc_dir = Path(directories["OMIE_HISTORICAL_PDBC"])
     return get_historical_energy_buy(engine, omie_pdbc_dir, verbose, dry_run)
 
+
 def main_update_energy_buy(verbose, dry_run):
-    engine = None if dry_run else create_engine(local_db['dbapi'])
-    omie_temp_dir = Path(directories['OMIE_TEMP_PDBC'])
+    engine = None if dry_run else create_engine(local_db["dbapi"])
+    omie_temp_dir = Path(directories["OMIE_TEMP_PDBC"])
     return update_energy_buy(engine, omie_temp_dir, verbose, dry_run)
 
+
 def main_update_neuroenergia(verbose, dry_run):
-    engine = None if dry_run else create_engine(local_db['dbapi'])
-    neuro_dir = directories['NEUROENERGIA_TEMP']
-    return update_neuroenergia(engine,neuro_dir,verbose,dry_run)
+    engine = None if dry_run else create_engine(local_db["dbapi"])
+    neuro_dir = directories["NEUROENERGIA_TEMP"]
+    return update_neuroenergia(engine, neuro_dir, verbose, dry_run)
+
 
 def main_update_conversations(verbose, dry_run):
-    engine = create_engine(local_db['dbapi'])
-    hs_app_id = helpscout_api['app_id']
-    hs_app_secret = helpscout_api['app_secret']
+    engine = create_engine(local_db["dbapi"])
+    hs_app_id = helpscout_api["app_id"]
+    hs_app_secret = helpscout_api["app_secret"]
     return update_hs_conversations(engine, hs_app_id, hs_app_secret, verbose, dry_run)
 
+
 def main_pipe_hourly_energy_budget(verbose, dry_run):
-    engine = None if dry_run else create_engine(local_db['dbapi'])
+    engine = None if dry_run else create_engine(local_db["dbapi"])
     return pipe_hourly_energy_budget(engine, verbose)
 
+
 def main_pipe_omie_garantia(verbose, dry_run):
-    engine = None if dry_run else create_engine(local_db['dbapi'])
+    engine = None if dry_run else create_engine(local_db["dbapi"])
     return pipe_omie_garantia(engine)
 
 
 function_list = {
-    'meff_update_closing_prices_day': main_update_closing_prices_day,
-    'meff_update_closing_prices_month': main_update_closing_prices_month,
-    'omie_get_historical_hour_price': main_get_historical_hour_price,
-    'omie_update_latest_hour_price': main_update_omie_latest_hour_price,
-    'omie_update_historical_hour_price': main_update_historical_hour_price,
-    'omie_get_historical_energy_buy' : main_get_historical_energy_buy,
-    'omie_update_energy_buy' : main_update_energy_buy,
-    'neuro_update_energy_prediction' : main_update_neuroenergia,
-    'hs_update_conversations' : main_update_conversations,
-    'pipe_hourly_energy_budget' : main_pipe_hourly_energy_budget,
-    'pipe_omie_garantia' : main_pipe_omie_garantia,
+    "meff_update_closing_prices_day": main_update_closing_prices_day,
+    "meff_update_closing_prices_month": main_update_closing_prices_month,
+    "omie_get_historical_hour_price": main_get_historical_hour_price,
+    "omie_update_latest_hour_price": main_update_omie_latest_hour_price,
+    "omie_update_historical_hour_price": main_update_historical_hour_price,
+    "omie_get_historical_energy_buy": main_get_historical_energy_buy,
+    "omie_update_energy_buy": main_update_energy_buy,
+    "neuro_update_energy_prediction": main_update_neuroenergia,
+    "hs_update_conversations": main_update_conversations,
+    "pipe_hourly_energy_budget": main_pipe_hourly_energy_budget,
+    "pipe_omie_garantia": main_pipe_omie_garantia,
 }
 
-# TODO use logging instead of -v
-@click.command()
-@click.option('-f','--function', type=str, default=None, help='Choose which function you want to run.', multiple=True)
-@click.option('-v','--verbose', default=2, count=True)
-@click.option('-l','--list-functions', default=False, is_flag=True)
-@click.option('-s','--dry-run', default=False, is_flag=True, help='Show dataframes but dont save to db')
-def dispatch(function, verbose, list_functions, dry_run):
 
+@click.command()
+@click.option("-f", "--function", type=str, default=None, help="Choose which function you want to run.", multiple=True)
+@click.option("-v", "--verbose", default=2, count=True)
+@click.option("-l", "--list-functions", default=False, is_flag=True)
+@click.option("-s", "--dry-run", default=False, is_flag=True, help="Show dataframes but dont save to db")
+def dispatch(function, verbose, list_functions, dry_run):
     if verbose > 1:
-        print(f"[{datetime.datetime.now()}] Start operations")
+        logger.info("Start operations")
 
     if list_functions:
-        print(f'Available functions {list(function_list.keys())}')
+        logger.info(f"Available functions {list(function_list.keys())}")
         return 0
 
     results = []
 
     for one_function in function:
-        print(f'Running {one_function}')
+        logger.info(f"Running {one_function}")
         operation_function = function_list.get(one_function, None)
 
         if operation_function:
             result = operation_function(verbose, dry_run)
             if result != 0 and verbose > 0 or result and verbose > 1:
-                print(f'{function} ended with result {result}')
+                logger.info(f"{function} ended with result {result}")
         else:
             if verbose > 0:
-                print(f'{function} not found. options: {list(function_list.keys())}')
+                logger.info(f"{function} not found. options: {list(function_list.keys())}")
             result = -1
 
         results.append(result)
 
     if verbose > 1:
-        print(results)
-        print(f"[{datetime.datetime.now()}] Job's Done, Have a Nice Day")
+        logger.info(results or "No results to show")
+        logger.info("Job's Done, Have a Nice Day")
     return results
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     results = dispatch()
     final_result = min(results)
     sys.exit(final_result)
