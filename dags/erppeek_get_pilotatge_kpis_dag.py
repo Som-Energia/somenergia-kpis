20233d7aecf18a56c3bfde4cc5ef0a3cfeaa744a
Diego Quintana <daquintanav@gmail.com>
2023-07-18 11:21:16 +0200

    fix: add get_random_moll() to dags (#15)
    also run black and isort on files

--- dags/erppeek_get_pilotatge_kpis_dag.py
+++ dags/erppeek_get_pilotatge_kpis_dag.py
@@ -1,58 +1,79 @@
-from airflow import DAG
-from airflow.providers.docker.operators.docker import DockerOperator
-from util_tasks.t_branch_pull_ssh import build_branch_pull_ssh_task
-from util_tasks.t_git_clone_ssh import build_git_clone_ssh_task
-from util_tasks.t_check_repo import build_check_repo_task
-from util_tasks.t_update_docker_image import build_update_image_task
-from docker.types import Mount, DriverConfig
+import random
 from datetime import datetime, timedelta
+
+from airflow import DAG
 from airflow.models import Variable
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import DriverConfig, Mount
+from util_tasks.t_branch_pull_ssh import build_branch_pull_ssh_task
+from util_tasks.t_check_repo import build_check_repo_task
+from util_tasks.t_git_clone_ssh import build_git_clone_ssh_task
+from util_tasks.t_update_docker_image import build_update_image_task
 
 my_email = Variable.get("fail_email")
 addr = Variable.get("repo_server_url")
 
-args= {
-  'email': my_email,
-  'email_on_failure': True,
-  'email_on_retry': False,
-  'retries': 5,
-  'retry_delay': timedelta(minutes=5),
+
+def get_random_moll() -> str:
+    available_molls = Variable.get("available_molls").split()
+    return random.choice(available_molls)
+
+
+args = {
+    "email": my_email,
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 5,
+    "retry_delay": timedelta(minutes=5),
 }
 
 nfs_config = {
-    'type': 'nfs',
-    'o': f'addr={addr},nfsvers=4',
-    'device': ':/opt/airflow/repos'
+    "type": "nfs",
+    "o": f"addr={addr},nfsvers=4",
+    "device": ":/opt/airflow/repos",
 }
 
-driver_config = DriverConfig(name='local', options=nfs_config)
-mount_nfs = Mount(source="local", target="/repos", type="volume", driver_config=driver_config)
+driver_config = DriverConfig(name="local", options=nfs_config)
+mount_nfs = Mount(
+    source="local", target="/repos", type="volume", driver_config=driver_config
+)
 
-with DAG(dag_id='erppeek_get_pilotatge_kpis_daily_dag', start_date=datetime(2022,9,1), schedule_interval='0 3 * * *', catchup=False, tags=["ERPPeek", "Extract"], default_args=args) as dag:
+with DAG(
+    dag_id="erppeek_get_pilotatge_kpis_daily_dag",
+    start_date=datetime(2022, 9, 1),
+    schedule_interval="0 3 * * *",
+    catchup=False,
+    tags=["ERPPeek", "Extract"],
+    default_args=args,
+) as dag:
+    repo_name = "somenergia-kpis"
 
-    repo_name = 'somenergia-kpis'
+    sampled_moll = get_random_moll()
 
     task_check_repo = build_check_repo_task(dag=dag, repo_name=repo_name)
     task_git_clone = build_git_clone_ssh_task(dag=dag, repo_name=repo_name)
-    task_branch_pull_ssh = build_branch_pull_ssh_task(dag=dag, task_name='erppeek_get_pilotatge_kpis_daily', repo_name=repo_name)
+    task_branch_pull_ssh = build_branch_pull_ssh_task(
+        dag=dag, task_name="erppeek_get_pilotatge_kpis_daily", repo_name=repo_name
+    )
     task_update_image = build_update_image_task(dag=dag, repo_name=repo_name)
 
-
     get_conversations_task = DockerOperator(
-        api_version='auto',
-        task_id='erppeek_get_pilotatge_kpis_daily',
-        docker_conn_id='somenergia_registry',
-        image='{}/{}-requirements:latest'.format('{{ conn.somenergia_registry.host }}',repo_name),
-        working_dir=f'/repos/{repo_name}',
+        api_version="auto",
+        task_id="erppeek_get_pilotatge_kpis_daily",
+        docker_conn_id="somenergia_registry",
+        image="{}/{}-requirements:latest".format(
+            "{{ conn.somenergia_registry.host }}", repo_name
+        ),
+        working_dir=f"/repos/{repo_name}",
         command='python3 -m datasources.erppeek.filtered_models_single_kpis "{{ var.value.dades_prod_db }}" "daily" \
                 "{{ var.value.erp_server_prod_ro }}" "{{ var.value.erp_database }}" "{{ var.value.erp_user_airflow }}" \
                 "{{ var.value.erp_pass_airflow_secret }}" "{{ var.value.schema_kpis_dades_prod_db }}"',
-        docker_url=Variable.get("generic_moll_url"),
+        docker_url=sampled_moll,
         mounts=[mount_nfs],
         mount_tmp_dir=False,
         auto_remove=True,
         retrieve_output=True,
-        trigger_rule='none_failed',
+        trigger_rule="none_failed",
     )
 
     task_check_repo >> task_git_clone
--- dags/hs_get_tags_dag.py
+++ dags/hs_get_tags_dag.py
@@ -1,56 +1,78 @@
-from airflow import DAG
-from airflow.providers.docker.operators.docker import DockerOperator
-from util_tasks.t_branch_pull_ssh import build_branch_pull_ssh_task
-from util_tasks.t_git_clone_ssh import build_git_clone_ssh_task
-from util_tasks.t_check_repo import build_check_repo_task
-from util_tasks.t_update_docker_image import build_update_image_task
-from docker.types import Mount, DriverConfig
+import random
 from datetime import datetime, timedelta
+
+from airflow import DAG
 from airflow.models import Variable
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import DriverConfig, Mount
+from util_tasks.t_branch_pull_ssh import build_branch_pull_ssh_task
+from util_tasks.t_check_repo import build_check_repo_task
+from util_tasks.t_git_clone_ssh import build_git_clone_ssh_task
+from util_tasks.t_update_docker_image import build_update_image_task
 
 my_email = Variable.get("fail_email")
 addr = Variable.get("repo_server_url")
 
-args= {
-  'email': my_email,
-  'email_on_failure': True,
-  'email_on_retry': False,
-  'retries': 5,
-  'retry_delay': timedelta(minutes=5),
+
+def get_random_moll() -> str:
+    available_molls = Variable.get("available_molls").split()
+    return random.choice(available_molls)
+
+
+args = {
+    "email": my_email,
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 5,
+    "retry_delay": timedelta(minutes=5),
 }
 
 nfs_config = {
-    'type': 'nfs',
-    'o': f'addr={addr},nfsvers=4',
-    'device': ':/opt/airflow/repos'
+    "type": "nfs",
+    "o": f"addr={addr},nfsvers=4",
+    "device": ":/opt/airflow/repos",
 }
 
-driver_config = DriverConfig(name='local', options=nfs_config)
-mount_nfs = Mount(source="local", target="/repos", type="volume", driver_config=driver_config)
+driver_config = DriverConfig(name="local", options=nfs_config)
+mount_nfs = Mount(
+    source="local", target="/repos", type="volume", driver_config=driver_config
+)
 
-with DAG(dag_id='hs_get_tags_dag', start_date=datetime(2022,6, 15), schedule_interval='@hourly', catchup=True, tags=["Helpscout", "Extract"], default_args=args) as dag:
+with DAG(
+    dag_id="hs_get_tags_dag",
+    start_date=datetime(2022, 6, 15),
+    schedule_interval="@hourly",
+    catchup=True,
+    tags=["Helpscout", "Extract"],
+    default_args=args,
+) as dag:
+    repo_name = "somenergia-kpis"
 
-    repo_name = 'somenergia-kpis'
+    sampled_moll = get_random_moll()
 
     task_check_repo = build_check_repo_task(dag=dag, repo_name=repo_name)
     task_git_clone = build_git_clone_ssh_task(dag=dag, repo_name=repo_name)
-    task_branch_pull_ssh = build_branch_pull_ssh_task(dag=dag, task_name='hs_get_tags', repo_name=repo_name)
+    task_branch_pull_ssh = build_branch_pull_ssh_task(
+        dag=dag, task_name="hs_get_tags", repo_name=repo_name
+    )
     task_update_image = build_update_image_task(dag=dag, repo_name=repo_name)
 
     get_tags_task = DockerOperator(
-        api_version='auto',
-        task_id='hs_get_tags',
-        docker_conn_id='somenergia_registry',
-        image='{}/{}-requirements:latest'.format('{{ conn.somenergia_registry.host }}',repo_name),
-        working_dir=f'/repos/{repo_name}',
+        api_version="auto",
+        task_id="hs_get_tags",
+        docker_conn_id="somenergia_registry",
+        image="{}/{}-requirements:latest".format(
+            "{{ conn.somenergia_registry.host }}", repo_name
+        ),
+        working_dir=f"/repos/{repo_name}",
         command='python3 -m datasources.helpscout.hs_get_tags "{{ data_interval_start }}" "{{ data_interval_end }}" \
                 "{{ var.value.puppis_prod_db}}" "{{ var.value.helpscout_api_id}}" "{{ var.value.helpscout_api_secret}}"',
-        docker_url=Variable.get("generic_moll_url"),
+        docker_url=sampled_moll,
         mounts=[mount_nfs],
         mount_tmp_dir=False,
         auto_remove=True,
         retrieve_output=True,
-        trigger_rule='none_failed',
+        trigger_rule="none_failed",
     )
 
     task_check_repo >> task_git_clone
