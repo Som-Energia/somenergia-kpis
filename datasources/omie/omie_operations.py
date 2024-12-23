ff2db35911e6ab3bbf78a622687d38537ea6c756
Pol Monsó Purtí <56979418+polmonso@users.noreply.github.com>
2024-12-23 12:10:57 +0100

    fix: remove html entities from url given that now produce error 500 on omie.es (#18)

--- datasources/omie/omie_operations.py
+++ datasources/omie/omie_operations.py
@@ -58,7 +58,7 @@ def get_historical_hour_price(engine, verbose=2, dry_run=False):
 
     filetype = 'marginalpdbc'
     hist_files = get_file_list(filetype)
-    file_base_url = f'https://www.omie.es/es/file-download?parents%5B0%5D={filetype}&filename='
+    file_base_url = f'https://www.omie.es/es/file-download?parents={filetype}&filename='
 
     total = len(hist_files['Nombre'])
 
@@ -92,7 +92,7 @@ def update_latest_hour_price(engine, verbose=2, dry_run=False):
 
     filetype = 'marginalpdbc'
     hist_files = get_file_list(filetype)
-    file_base_url = f'https://www.omie.es/es/file-download?parents%5B0%5D={filetype}&filename='
+    file_base_url = f'https://www.omie.es/es/file-download?parents={filetype}&filename='
 
     file_name = hist_files['Nombre'][0]
     pathfile = file_base_url + file_name
@@ -124,7 +124,7 @@ def update_historical_hour_price(engine, verbose=2, dry_run=False):
 
     filetype = 'marginalpdbc'
     hist_files = get_file_list(filetype)
-    file_base_url = f'https://www.omie.es/es/file-download?parents%5B0%5D={filetype}&filename='
+    file_base_url = f'https://www.omie.es/es/file-download?parents={filetype}&filename='
 
     file_name = hist_files['Nombre'][0]
     pathfile = file_base_url + file_name
@@ -193,7 +193,7 @@ def update_energy_buy_fromweb(engine, verbose=2, dry_run=False):
 
     filetype = 'pdbc'
     hist_files = get_file_list(filetype)
-    file_base_url = f'https://www.omie.es/es/file-download?parents%5B0%5D={filetype}&filename='
+    file_base_url = f'https://www.omie.es/es/file-download?parents={filetype}&filename='
 
     file_name = hist_files['Nombre'][0]
 
