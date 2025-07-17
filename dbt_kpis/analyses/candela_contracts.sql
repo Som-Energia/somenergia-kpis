8f79451dee26bbb97eb1322945f3d9b2aade304e
Roger <roger.sanjaume@somenergia.coop>
2025-07-17 17:09:14 +0200

    Remove moved models to new repo

--- dbt_kpis/analyses/candela_contracts.sql
+++
@@ -1,7 +0,0 @@
-select
-	count(p.id),
-	count(*) filter (where p.active and p.data_baixa is null and p.state = 'activa') as contracte_actiu,
-    count(*) filter (where donatiu) as with_donatiu
-from
-    {{ source('erp', 'giscedata_polissa') }} as p
-    where soci = 190794
